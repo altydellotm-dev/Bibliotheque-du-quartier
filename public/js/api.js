869c260566967f3c38309f4a5e3283d0f7b6eb58
altydellotm-dev <altydellotm@gmail.com>
2026-09-17 14:13:43 +0100

    Change API_URL to relative path

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = '/api';
 
 // Fonction generique pour appeler l'API et gerer les erreurs
 async function appelApi(endpoint, options = {}) {
